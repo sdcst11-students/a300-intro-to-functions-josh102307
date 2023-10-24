50e73d466a47f2b093405e6b9320a351d3f600d2
josh102307 <144732172+josh102307@users.noreply.github.com>
2023-10-24 13:01:01 -0700

    i did the assignment

--- task1.py
+++ task1.py
@@ -1,6 +1,14 @@
 def A():
-
+    print("hello")
 def B():
-
-
+    print("how are you")
 def C():
+    print("hi")
+
+letter = input("enter the letter A,B,C: ")
+if letter == "A":
+    A()
+if letter == "B":
+    B()
+if letter == "C":
+    C()
--- task2.py
+++ task2.py
@@ -8,3 +8,23 @@ displays instructions and how to play
 game()
 plays the games
 """
+
+import random
+
+def title():
+    print("pick a number between 1 and 10 and see if it matches the radndom number")
+
+def game():
+    num = int(input("number: "))
+    randNum = int(random.randint(1,10))
+    if num == randNum:
+        print("correct")
+    else:
+        print(f"Wrong, the correct number was {randNum}")
+
+title()
+
+game()
+
+    
+
